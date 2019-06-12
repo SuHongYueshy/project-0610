8e60590d396a7e81c6eea7336919157521035a13
thu <mingfei.net@gmail.com>
2019-06-12 15:25:00 +0800

    Update

--- python_advanced/generator.py
+++ python_advanced/generator.py
@@ -8,20 +8,31 @@
 
 list_a = list(range(1, 11))
 
-print(list_a)
+# print(list_a)
 
-list_a = [x**2 for x in range(1, 11)]
+# list_a = [x**2 for x in range(1, 11)]
 
 g_a = (x**2 for x in range(1, 5))
 
-print(list_a)
-print(type(g_a))
+# print(list_a)
+# print(type(g_a))
 
-print(next(g_a))
-print(next(g_a))
-print(next(g_a))
-print(next(g_a))
+# print(next(g_a))
+# print(next(g_a))
+# print(next(g_a))
+# print(next(g_a))
 # print(next(g_a))
 
+for n in list_a:
+    print('n:', n)
+
 for n in g_a:
-    print(n)
+    print('n:', n)
+
+print('----------')
+
+for n in list_a:
+    print('n:', n)
+
+for n in g_a:
+    print('n:', n)
